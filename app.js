093adf6c24b2d93546bab8cf70acb7faed7528a5
Jaiditya Batra <jaiditya7318@gmail.com>
2023-07-09 23:14:47 +0530

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const exp = require("constants");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -14,10 +15,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const sercet = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: sercet, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
